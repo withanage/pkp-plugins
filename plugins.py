ce69a0c0860cf1853eba762e2cf50a92bf00e30f
withanage <dulip.withanage@gmail.com>
2020-05-15 20:55:49 +0200

    update

--- plugins.py
+++ plugins.py
@@ -1,9 +1,25 @@
 import xml.etree.ElementTree as ET
 from string import Template
+import requests
+import sys
+from collections import OrderedDict
 
 NS = '{http://pkp.sfu.ca}'
+
+
+def get_xml():
+    r = requests.get('https://pkp.sfu.ca/ojs/xml/plugins.xml')
+    if r.status_code == 200:
+        file = open("plugins.xml", "w")
+        file.write(r.content.decode("utf-8"))
+        file.close()
+    else:
+        sys.exit()
+
+
+
 root = ET.parse('plugins.xml').getroot()
-s = Template('|$plugin|$name|$applications|$category|$summary|$homepage')
+s = Template('|$plugin|$applications|$summary')
 rows =[]
 
 def ns(s):
@@ -12,17 +28,20 @@ def ns(s):
 def header():
     rows.append('### OJS / OMP / OPS Plugins - list')
 
-    rows.append('|Plugin|Name|OJS|OMP|OPS|category|Summary|Homepage')
-    rows.append('|---|---|---|---|---|---|---|---|')
+    rows.append('|Plugin|OJS|OMP|OPS|Summary')
+    rows.append('|---|---|---|---|---')
 
 def plugins():
+    plugins = {}
     for product in root:
+        rank = 0
         category = product.attrib.get('category')
-        plugin = product.attrib.get('product')
-        applications = ['', '', '']
+        p = product.attrib.get('product')
+        plugins[p] = {}
+        applications = ['-', '-', '-']
         for metadata in product:
             if metadata.tag == ns('name'): name = metadata.text
-            if metadata.tag == ns('homepage'): homepage = '[{}]({})'.format(name, metadata.text)
+            if metadata.tag == ns('homepage'): plugin = '[{}]({})'.format(name, metadata.text)
             if metadata.tag == ns('summary'):  summary = metadata.text
 
             if metadata.tag == ns('release'):
@@ -30,16 +49,28 @@ def plugins():
                     if release.tag == ns('compatibility'):
                         for i, val in enumerate(['ojs2', 'omp', 'ops']):
                             if release.attrib.get('application') == val:
+                                rank += 1
                                 applications[i] = ':ok:'
-        row = s.substitute(plugin=plugin, category=category, name=name, homepage=homepage,summary=summary,
+        row = s.substitute(plugin=plugin,summary=summary,
                            applications='|'.join(applications))
-        rows.append(row)
+        plugins[p]['row'] = row
+        plugins[p]['rank'] = rank
+    return  plugins
 
 def main():
+    get_xml()
     header()
-    plugins()
+
+
+
     file = open("README.md", "w")
     file.write('\n'.join(rows))
+    file.write('\n')
+    for i in sorted(plugins().items()):
+        file.write(i[1].get('row'))
+        file.write('\n')
+
+
     file.close()
 
 if __name__ == "__main__":
